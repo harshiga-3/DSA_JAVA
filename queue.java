7e58fd93dfd07e9bbe3951c38ae031b2f613eebe
harsh <harshigadevi@gmail.com>
2026-02-17 11:38:48 +0530

    add

--- queue.java
+++ queue.java
@@ -1,78 +1,106 @@
-
 import java.util.*;
+class Queue{
+    int max;
+    int rear;
+    int front;
+    int []arr;
+    Queue(int size){
+      front=0;
+      rear=-1;
+      max=size;
+      arr=new int[max];
+      
+    }
+    
+    public  void enqueue(int x)
+    {
+        if(rear==max-1)
+        {
+            System.out.println("queue in overflow");
+        }
+        else{
+            rear++;
+            arr[rear]=x;
+        }
+    }
+    
+    public  void dequeue()
+    {
+        if(rear<front)
+        {
+            System.out.println("queue is underflow");
+        }else{
+            System.out.println("delete: "+arr[front]);
+            front++;
+        }
+    }
+    
+    public  void peek()
+    {
+        if(front>rear)
+        {
+            System.out.println("queue is underflow");
+        }else{
+            System.out.println("peek"+arr[front]);
+        }
+    }
+    
+    public  void display(){
+        for(int i=front;i<=rear;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
 public class Main
 {
 	public static void main(String[] args) {
-		//System.out.println("Hello World");
-		Scanner sc=new Scanner(System.in);
-		
-		
-		System.out.println("enter total no of element");
-		
-		int []queue=new int[10];
-		int size=sc.nextInt();
-		
-		
-		int front= -1 ;int rear=-1;
-		
-		
-		for(int i=0;i<size;i++)
-		{
-		    
-		System.out.println("enter option");
-		
-	int 	opt=sc.nextInt();
-		
-		
-		switch (opt){
-		    case 1:
-		        if(rear ==queue.length -1){
-		            System.out.println("queue is full");
-		        }else{
-		        if(front == -1 ) front =0;
-		        System.out.println("enter insert");
-		        queue[++rear]=sc.nextInt();
-		        }
-		        
-		        break;
-		        case 2:
-		            if(front !=-1 &&  rear >= front ){
-		            int tail=queue[front];
-		            System.out.println("delete "+tail);
-		            front++;
-		            }
-		            else{
-		                System.out.println("queue is empty");
-		            }
-		            break;
-		            
-		            case 3:
-		                if(rear>=front && front != -1){
-		                System.out.println("front "+queue[front]);
-		                }
-		                else{
-		                    System.out.println("queue is empty");
-		                }
-		                break;
-		            
-		}}
-		
-		
-		
-		for( int i=front;i<=rear;i++)
-		{
-		    System.out.println(queue[i]);
-		}
-		
-		
-		
-		
-		                
-		
-		
-		
-		
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int op=sc.nextInt();
+	    Queue q=new Queue(n);
+/*	q.enqueue(10);
+	q.enqueue(20);
+	q.enqueue(30);
+	q.enqueue(40);
+	q.dequeue();
+	q.display();
+	q.peek();
+	q.display();*/
 	
-		
+	
+	while(op>0)
+	{
+	    System.out.println("enter option");
+	    
+	    int c=sc.nextInt();
+	    
+	    switch(c){
+	        case 1:
+	            int val=sc.nextInt();
+	            q.enqueue(val);
+	            break;
+	            
+	       case 2:
+	           q.dequeue();
+	           break;
+	           case 3:
+	               q.peek();
+	               break;
+	               
+	       case 4:
+	           q.display();
+	           break;
+	           
+	           default:
+	           System.out.println("not valid");
+	           
+	           op--;
+	    }
+	}
+	
+	
+	
+	
 	}
 }
